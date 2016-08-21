feb1d2c243d9888248a48e7a21367b2ddecdddcc
ErraticUnicorn <david.j.wert@gmail.com>
2016-08-21 18:06:38 -0500

    changes font urls to font-url

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,6 @@ module LukeWebsite
     config.active_record.raise_in_transactional_callbacks = true
     config.assets.enabled = true
     config.assets.paths << Rails.root.join('/app/assets/fonts')
+    config.assets.precompile += %w( .svg .eot .woff .ttf )
   end
 end
